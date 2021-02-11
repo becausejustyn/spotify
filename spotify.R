c56c7f645314c0c3e701bde40a700ceb4ea150c5
becausejustyn <77762513+becausejustyn@users.noreply.github.com>
2021-02-11 16:13:30 +1100

    Add files via upload

---
+++ spotify.R
@@ -0,0 +1,33 @@
+library(jsonlite)
+library(lubridate)
+library(gghighlight)
+library(spotifyr)
+library(tidyverse)
+library(knitr)
+library(ggplot2)
+library(plotly)
+
+streamHistory <- fromJSON("C:/Users/justy/Downloads/spotify-main/spotify-main/StreamingHistory0.json", flatten = TRUE)
+
+streamHistory 
+
+
+# Adding date, time and playcount
+
+mySpotify <- streamHistory %>% 
+  as_tibble() %>% 
+  mutate_at("endTime", ymd_hm) %>% 
+  mutate(endTime = endTime - hours(6)) %>% 
+  mutate(date = floor_date(endTime, "day") %>% 
+           as_date, seconds = msPlayed / 1000, minutes = seconds / 60) %>%
+  add_count(trackName) %>%
+  rename(playCount = n)
+
+#Moved columns  
+mySpotify <- mySpotify %>%
+  relocate(endTime, .after = minutes) %>%
+  relocate(playCount, .after = trackName) %>%
+  relocate(trackName, .before = artistName)
+
+
+    
